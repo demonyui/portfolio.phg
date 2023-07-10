9e5a087f7c5f5b539fd333bcf6f8e89ca13f73d6
Phùng Hồng Quân <135598758+phunghongquan94@users.noreply.github.com>
2023-07-10 11:06:25 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // ko lay code
-// document.addEventListener("keydown", function (event) {
-//     if (event.ctrlKey) {
-//         event.preventDefault();
-//     }
-//     if (event.keyCode == 123) {
-//         event.preventDefault();
-//     }
-// });
-// document.addEventListener('contextmenu',
-//     event => event.preventDefault()
-// );
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }
+    if (event.keyCode == 123) {
+        event.preventDefault();
+    }
+});
+document.addEventListener('contextmenu',
+    event => event.preventDefault()
+);
 // fix menu
 // document.addEventListener("DOMContentLoaded", function () {
 //     window.addEventListener('scroll', function () {
